aed3b499f652120e858287996c05449d78c030cc
Amós Pedro <amos.pedro1@gmail.com>
2024-05-31 22:03:14 -0300

    Escopo api funcionarios
    falta adicionar a conexão com o banco

---
+++ backend/server.js
@@ -0,0 +1,122 @@
+const express = require('express');
+
+const app = express();
+
+const cors = require('cors');
+
+app.use(cors({
+    origin: 'https://localhost:8000/', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    allowedHeaders: ['Content-Type', 'Authorization'] 
+  }));
+  
+
+app.use(express.json());
+
+let funcionarios = [
+    {
+        id: 1,
+        nome: "Amos",
+        cpf: "98978767567",
+        idade: "30",
+        cargo: "Desenvolvedor",
+        setor: "TI",
+        dataDeEntrada: "30/05/2024",
+        dataDeAtt: "30/05/2024"
+    },
+
+    {
+        id: 2,
+        nome: "Abrão",
+        cpf: "80870976757",
+        idade: "28",
+        cargo: "Desenvolvedor",
+        setor: "TI",
+        dataDeEntrada: "31/05/2024",
+        dataDeAtt: "31/05/2024"
+    },
+
+    {
+        id: 3,
+        nome: "José Ivan",
+        cpf: "98978767987",
+        idade: " 45",
+        cargo: "Desenvolvedor",
+        setor: "TI",
+        dataDeEntrada: "30/05/2024",
+        dataDeAtt: "30/05/2024"
+    }
+];
+
+
+app.get('/funcionarios', (req, res) => {
+    res.json(funcionarios);
+});
+
+
+app.get('/funcionarios/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const funcionario = funcionarios.find(f => f.id === id);
+
+    if (!funcionario) {
+        return res.status(404).json({ mensagem: "Funcionário não encontrado" });
+    }
+
+    res.json(funcionario);
+});
+
+
+app.post('/funcionarios', (req, res) => {
+    const novoFuncionario = {
+        id: funcionarios.length ? funcionarios[funcionarios.length - 1].id + 1 : 1,
+        nome: req.body.nome,
+        cpf: req.body.cpf,
+        idade: req.body.idade,
+        cargo: req.body.cargo,
+        setor: req.body.setor,
+        dataDeEntrada: req.body.dataDeEntrada,
+        dataDeAtt: new Date().toISOString().split('T')[0] 
+    };
+
+    funcionarios.push(novoFuncionario);
+    res.status(201).json({mensagem: "Funcionario adicionado com sucesso!" });
+
+});
+
+
+app.put('/funcionarios/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const funcionario = funcionarios.find(f => f.id === id);
+
+    if (!funcionario) {
+        return res.status(404).json({ mensagem: "Funcionário não encontrado"});
+    }
+
+    funcionario.nome = req.body.nome || funcionario.nome;
+    funcionario.cpf = req.body.cpf || funcionario.cpf;
+    funcionario.idade = req.body.idade || funcionario.idade;
+    funcionario.cargo = req.body.cargo || funcionario.cargo;
+    funcionario.setor = req.body.setor || funcionario.setor;
+    funcionario.dataDeAtt = new Date().toISOString().split('T')[0];
+
+    res.json({mensagem: "Funcionario atualizado com sucesso"});
+
+});
+
+
+app.delete('/funcionarios/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const index = funcionarios.findIndex(f => f.id === id);
+
+    if (index === -1) {
+        return res.status(404).json({ mensagem: "Funcionário não encontrado" });
+    }
+
+    const funcionarioRemovido = funcionarios.splice(index, 1);
+    res.json({mensagem: "Funcionario removiso com sucesso"});
+});
+
+
+app.listen(8000, () => {
+    console.log('Servidor iniciado na porta 8000');
+});
